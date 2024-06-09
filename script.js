bc0e1772b0e8263659379b15fe067c77610a8efe
horlahuk <olaitan.u.k@gmail.com>
2024-06-09 11:30:31 +0100

    add logic for computer choice

---
+++ script.js
@@ -0,0 +1,21 @@
+// LOGIC FOR COMPUTER CHOICE
+
+function getComputerChoice() {
+  let decision;
+  let num = Math.floor(Math.random() * 10);
+  if (num <= 3) {
+    decision = "Rock";
+  } else if (num == 4 || num <= 7) {
+    decision = "Paper";
+  } else {
+    decision = "Scissors";
+  }
+  // console.log(num);
+  // console.log(decision);
+}
+
+getComputerChoice();
+
+// LOGIC FOR HUMAN CHOICE
+
+function getHumanChoice() {}
